ae5ac576ba93fc386282e9adad2964001821e172
AporSz <szakacsapor2004@gmail.com>
2026-04-30 16:49:26 +0300

    progress? maybe?

---
+++ hw5/4.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from numpy.fft import fft, ifft, fftshift, ifftshift, fftfreq
+
+dt = 0.001
+fs = 1/dt
+
+# fmax > 0
+fmax = 4
+N = 4096
+
+f = fftfreq(N, dt)
+
+X = np.zeros(N)
+X[abs(f) < fmax]  = (fmax ** 2 - f ** 2)[abs(f) < fmax]
+
+# a
+plt.plot(fftshift(f), fftshift(X))
+plt.xlim(-10, 10)
+plt.show()
+
+x = fs * np.real(ifft(X))
+
+t = fftfreq(N, fs/N)
+
+# b
+plt.plot(fftshift(t), fftshift(x))
+plt.show()
+
+m = 16
+T = dt * m
+xhat = x[::m]
+tsamp = t[::m]
+
+# c
+plt.plot(fftshift(t), fftshift(x))
+plt.plot(fftshift(tsamp), fftshift(xhat), '.')
+plt.show()
+
