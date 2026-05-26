54665998db255c5824ebd3780ce33efe440e4f7f
AporSz <szakacsapor2004@gmail.com>
2026-05-27 00:05:37 +0300

    project chaos

--- project/unfiltered.py
+++ project/unfiltered.py
@@ -1,15 +1,53 @@
 import numpy as np
 
 def load(path):
-    data = []
+    data = {
+        "time": [],
+        "pressure_top" : [],
+        "humidity_top" : [],
+        "temperature_top" : [],
+        "pressure_bottom" : [],
+        "humidity_bottom" : [],
+        "temperature_bottom" : [],
+        "co2_main" : [],
+        "co2_side" : [],
+    }
     with open(path) as f:
-        for line in f:
-            l = line.strip().split()
-            t = float(l[0])
-            
+        while not f.closed:
+            try:
+                line = f.readline()
+                l = line.strip().split()
+                t = float(l[0])
 
-            print(l)
+                line = f.readline()
+                l = line.strip()[2:].split()
+                if len(l) <= 2:
+                    raise ValueError("Line not containing enough data")
 
-    return None
+                data["time"].append(t)
+
+                pressure, humidity, temperature = float(l[0]), float(l[1]), float(l[2])
+                data["pressure_top"].append(pressure)
+                data["humidity_top"].append(humidity)
+                data["temperature_top"].append(temperature)
+
+                pressure, humidity, temperature = float(l[3]), float(l[4]), float(l[5])
+                data["pressure_bottom"].append(pressure)
+                data["humidity_bottom"].append(humidity)
+                data["temperature_bottom"].append(temperature)
+
+                for i in range(6, 6 + 40):
+                    co2 = float(l[i])
+                    data["co2_main"].append(co2)
+
+                for i in range(6 + 40, 6 + 40 + 8):
+                    c02 = float(l[i])
+                    data["co2_side"].append(c02)
+            except Exception as e:
+                # print(e)
+                continue
+
+        return data
 
 data = load('data/1_CO2_raw_data/new_device_column1.txt')
+print(data)
