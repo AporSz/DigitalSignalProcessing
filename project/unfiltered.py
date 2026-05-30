533f76c343fbd11ed6a2038a46dbf904f8e392a8
AporSz <szakacsapor2004@gmail.com>
2026-05-30 23:28:00 +0300

    better plots

--- project/unfiltered.py
+++ project/unfiltered.py
@@ -7,26 +7,43 @@ from project.utils.utils import load
 
 matplotlib.use('TkAgg')
 
-def plot_filtered(time, values):
+def plot_filtered(time, values, color):
     smoothed = savgol_filter(values, 51, 3)
-    plt.plot(time, smoothed)
+    plt.plot(time, smoothed, color = color)
 
-def plot_sensor(sensor, filtered = True):
+def add_vertical_line(data, sensor):
+    ymin = np.array(data[sensor]).flatten().min()
+    ymax = np.array(data[sensor]).flatten().max()
+    xmin = np.array(data["time"]).flatten().min()
+    xmax = np.array(data["time"]).flatten().max()
+    vertical_lines = np.arange(xmin, xmax)[::86400]
+    plt.vlines(vertical_lines, ymin=ymin, ymax=ymax, colors='grey')
+
+def plot_sensor(data, sensor, filtered = False, mark_days = False, color = "blue"):
+    if mark_days:
+        add_vertical_line(data, sensor)
+
     if np.array(data[sensor]).shape.__len__() == 1:
         if filtered:
-            plot_filtered(data["time"], data[sensor])
+            plot_filtered(data["time"], data[sensor], color = color)
         else:
-            plt.plot(data["time"], data[sensor])
+            plt.plot(data["time"], data[sensor], color = color)
+
         return
+
+    cmap = plt.get_cmap('plasma')
+    colors = cmap(np.linspace(1, 0, len(data[sensor])))
 
     for i in range(len(data[sensor])):
         if filtered:
-            plot_filtered(data["time"], data[sensor][i])
+            plot_filtered(data["time"], data[sensor][i], color = colors[i])
         else:
-            plt.plot(data["time"], data[sensor][i])
+            plt.plot(data["time"], data[sensor][i], color = colors[i])
 
-data = load('data/1_CO2_raw_data/new_device_column1.txt', limit=1000000)
+N = 1000000
 
-plot_sensor("pressure_top", True)
+sensor_values = load('data/1_CO2_raw_data/new_device_column1.txt', limit = N)
+
+plot_sensor(sensor_values,"co2_main", filtered = True, mark_days = True)
 
 plt.show()
